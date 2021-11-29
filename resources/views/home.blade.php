df711415d135c05510927f599db27206e6b879de
rick98788 <51737436+rick98788@users.noreply.github.com>
2021-11-29 12:22:28 +0100

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,17 +60,6 @@
                     </div>
                 </div>
             </div>
-
-            <div class="col-12 col-xl-12">
-                <div class="col-12 px-0">
-                    <div class="card border-0 shadow">
-                        <div class="card-body">
-                            <h2 class="fs-5 fw-bold mb-1">{{ __('Dashboard') }}</h2>
-                            <p>{{ __('You are logged in!') }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 @endsection
--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -3,6 +3,35 @@
 @section('content')
     <div class="main py-4">
         <div class="row">
+            <div class="col-md-12">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h5 class="card-title">Zaterdag 1</h5>
+                            </div>
+                            <div class="col-md-6">
+                                <p>Trainer: Ron Jans</p>
+                            </div>
+                            <div class="col-md-6">
+                                <p>Leden: 12</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+{{--            @foreach()--}}
+            <div class="col-md-6">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <h5 class="card-title mb-0">Memphis Depay</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
